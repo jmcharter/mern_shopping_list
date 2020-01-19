ea79a88edb89d1f84a2c5fe54537ac0a8c24b490
Jon Charter <jmcharter@gmail.com>
2020-01-19 20:12:58 +0000

    created and added ShoppingList component

---
+++ client/src/components/ShoppingList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import uuid from 'uuid';
+
+class ShoppingList extends Component {
+    state = {
+        items: [
+            { id: uuid(), name: 'Eggs' },
+            { id: uuid(), name: 'Bacon' },
+            { id: uuid(), name: 'Sausages' },
+            { id: uuid(), name: 'Black Pudding' }
+        ]
+    }
+
+    render() {
+        const { items } = this.state;
+        return (
+            <Container>
+                <Button
+                    color="dark"
+                    style={{ marginBottom: '2rem' }}
+                    onClick={() => {
+                        const name = prompt('Enter item');
+                        if (name) {
+                            this.setState(state => ({
+                                items: [...state.items, { id: uuid(), name }]
+                            }))
+                        }
+                    }}
+                >
+                    Add Item
+                </Button>
+
+                <ListGroup>
+                    <TransitionGroup className="shopping-list">
+                        {items.map(({ id, name }) => (
+                            <CSSTransition key={id} timeout={500} classNames="fade">
+                                <ListGroupItem>
+                                    <Button
+                                        className="remove-btn"
+                                        size="sm"
+                                        onClick={() => {
+                                            this.setState(state => ({
+                                                items: state.items.filter(item => item.id !== id)
+                                            }));
+                                        }}
+                                    >&times;</Button>
+                                    {name}
+                                </ListGroupItem>
+                            </CSSTransition>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        );
+    }
+}
+
+export default ShoppingList;
