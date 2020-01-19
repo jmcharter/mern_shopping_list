5645a01ef9aaf72d1c7054efb511f2a016bd4327
Jon Charter <jmcharter@gmail.com>
2020-01-19 18:31:00 +0000

    created a navbar component

---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink
+} from 'reactstrap';
+
+class AppNavbar extends Component {
+    state = {
+        isOpen: false
+    }
+
+    toggle = () => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar color="dark" dark expand="sm">
+                    <NavbarBrand href="/">Shopping List</NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.state.isOpen} navbar>
+                        <Nav className="ml-auto" navbar>
+                            <NavItem>
+                                <NavLink href="https://github.com/jmcharter">GitHub</NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        );
+    }
+}
+
+
+export default AppNavbar;
