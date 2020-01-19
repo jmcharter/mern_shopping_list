2d1bddf686af9a075be512b2103f713c578c0d05
Jon Charter <jmcharter@gmail.com>
2020-01-19 12:03:14 +0000

    added collection name

--- server.js
+++ server.js
@@ -2,18 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const items = require('./routes/api/items');
+
 const app = express();
 
 //Bodyparser middleware
 app.use(bodyParser.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = require('./config/keys').mongoURI; // Link to local mongo db
 
 // Connect to Mongo
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected!'))
     .catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
 
 const port = process.env.PORT || 5000;
 
